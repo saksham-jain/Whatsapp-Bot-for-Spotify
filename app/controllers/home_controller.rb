ca384a69036d47649f7a011884317595486a714b
Saksham Jain <jainsaksham16@gmail.com>
2020-12-17 15:00:54 +0530

    aa

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,7 @@ class HomeController < ApplicationController
 
   def logged_in
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    @user_id = spotify_user.id
+    @credentials = spotify_user.credentials
   end
 end
