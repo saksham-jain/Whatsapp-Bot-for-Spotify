30bf60de714deecfb9947600e52beee8e3370dc0
Saksham Jain <jainsaksham16@gmail.com>
2020-12-31 11:53:37 +0530

    added bootstrap classes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "e5d89eb3486d.ngrok.io"
+  config.hosts << "a439785d0d3f.ngrok.io"
 end
